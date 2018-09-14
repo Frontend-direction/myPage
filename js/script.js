178c71628218235789f703348b6b0f73b6925925
Vova Pankiv <vovapankiw@gmail.com>
2018-09-14 04:06:40 +0000

    Initial

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
- const burgerMenu  = document.querySelector('.menuBurger');
+$(function() {
+    $('.smooth').on('click', function(event) {
+        let target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 700);
+        }
+    });
+});
 
-burgerMenu.addEventListener('click', function () {
-      burgerMenu.classList.toggle('change');
-      
-})
+$(document).ready(function() {
 
+		const burgerMenu  = document.querySelector('.menuBurger');
+		burgerMenu.addEventListener('click', function () {
+		burgerMenu.classList.toggle('change');
+		})
+        /*Animation My way*/
+		$('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn'); 
+    },{
+        offset:'50%'
+    });
+     
+});
 
